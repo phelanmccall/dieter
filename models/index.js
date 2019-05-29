1adad896edbfcfc6879507811ebfc3797c86bf70
Phelan <phelanmccall@yahoo.com>
2019-05-28 20:30:59 -0700

    update db exports

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Auths: require("./auths")
+  Auths: require("./auths"),
+  Plans: require("./plans"),
+  Recipes: require("./recipes")
 };
