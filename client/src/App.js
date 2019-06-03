df8df96be8ca8d8de322e4ab3ef8982e35e47651
Phelan <phelanmccall@yahoo.com>
2019-06-02 20:59:52 -0700

    fixed title

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 
@@ -18,7 +17,7 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
+      <div>
         <Header></Header>
 
         <Router>
