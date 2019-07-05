0a507d38a77b34e364959d2964e34909a6522a25
Phelan McCall <phelanmccall@yahoo.com>
2019-07-05 15:54:21 -0700

    update api to split instructions differently

--- routes/index.js
+++ routes/index.js
@@ -212,7 +212,7 @@ router.route("/getRecipeById")
       console.log(response.data);
 
       let title = response.data.title;
-      let steps = response.data.instructions.split(". ");
+      let steps = response.data.instructions.split(".");
       let ingredients = response.data.extendedIngredients.map(function (val, ind) {
         return val.originalString;
       })
