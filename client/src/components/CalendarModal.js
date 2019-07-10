0b2ade6c0f1a0b5a8eb0e8dea12cc8223da8df01
Phelan McCall <phelanmccall@yahoo.com>
2019-07-10 15:33:46 -0700

    typo

--- client/src/components/CalendarModal.js
+++ client/src/components/CalendarModal.js
@@ -14,7 +14,7 @@ class CalendarModal extends Component {
     }
     getMonthPlans = () =>{
         let year = this.state.date.split("-")[0];
-        let month = this.state.date.split("-"[1];
+        let month = this.state.date.split("-")[1];
         axios.get("/meals/"+year+"/"+month).then((response)=>{
             console.log(response.data);
             this.setState({
